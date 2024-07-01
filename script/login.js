462d4e0e8146703a12a62f4a41a8778ef51f04d4
Gabisteimacher19 <108942376+Gabisteimacher19@users.noreply.github.com>
2024-07-01 08:51:03 -0300

    Add files via upload

--- script/login.js
+++ script/login.js
@@ -1,28 +1,26 @@
-const btnLogin = document.querySelector ("#btnLogin");
-const inputUsuario = document.querySelector ("#usario");
-const inputSenha = document.querySelector ("#senha");
+const btnLogin = document.querySelector("#btnLogin");
+const inputUsuario = document.querySelector("#usuario");
+const inputSenha = document.querySelector("#senha");
 let usuarioLogado = null;
 
 (() => {
-    usuarioLogado = localStorage.getItem("usario");
-    if(usuarioLogado){
-        window.location.href = "///tmp/guest-lvk0qb/Downloads/g243i2t-main/index.html";
+    usuarioLogado = localStorage.getItem("usuario");
+    if (usuarioLogado) {
+        window.location.href = "/home/aluno/Downloads/g243i2t-main/logado.html";
     }
 })();
 
-btnLogin.onclick = (e) =>{
-
+btnLogin.onclick = (e) => {
     e.preventDefault();
-
-    let usurio = inputUsuario.value;
+    let usuario = inputUsuario.value;
     let senha = inputSenha.value;
-    if(usuario){
-        if(usuario === "gabrielle"){
-        if(senha === "191207"){
-            localStorage.setItem("usuario" ,usiario);
-            window.location.href = "/logado.html";
-        }
-        }else{
+    if (usuario) {
+        if (usuario === "user") {
+            if (senha === "123") {
+                localStorage.setItem("usuario", usuario);
+                window.location.href = "/home/aluno/Downloads/g243i2t-main/logado.html";
+            }
+        } else {
             inputUsuario.focus();
         }
     }
